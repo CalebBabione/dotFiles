14d1195bec6c5ed647f31d5d034319eef180dd1e
cbabione <babione.caleb@gmail.com>
2025-09-21 02:36:02 -0500

    first upload

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require("config.keybinds") --Shortcuts
+require("config.lazy") --LazyPackageManager
+require("config.color") --Color
+require("config.lkb") --TelescopeKeyBinds
+require("config.defaultConfig") --default config
